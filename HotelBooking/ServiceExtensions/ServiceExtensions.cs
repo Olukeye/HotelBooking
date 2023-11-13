d4f9d4cae54f8a29f105b478bfc00e56a6f3e85a
Sunkanmi Olukeye <53058618+Olukeye@users.noreply.github.com>
2023-11-13 14:07:50 +0100

    Update ServiceExtensions.cs

--- HotelBooking/ServiceExtensions/ServiceExtensions.cs
+++ HotelBooking/ServiceExtensions/ServiceExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 
+
 namespace HotelBooking.Utils_OR_ServiceExtensions
 {
     public static class ServiceExtensions
